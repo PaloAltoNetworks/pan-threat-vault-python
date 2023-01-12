e6d8097ac096251015d95e7f4c8ff840102ec196
Kevin Steves <kevin.steves@pobox.com>
2023-01-11 19:18:47 -0800

    Add test for release_notes(version='latest').

--- tests/test_aio_release_notes.py
+++ tests/test_aio_release_notes.py
@@ -5,14 +5,20 @@ from . import mixin
 
 class ThreatVaultApiTest(mixin.AioMixin, unittest.IsolatedAsyncioTestCase):
     async def test_01(self):
-        version = '8549-7323'
-        resp = await self.api.release_notes(type='content',
-                                            version=version)
-        self.assertEqual(resp.status, 200)
-        x = await resp.json()
-        self.assertEqual(x['message'], 'Successful')
-        self.assertTrue(x['success'])
-        self.assertEqual(x['count'], 1)
-        self.assertEqual(x['count'], len(x['data']))
-        item = x['data'][0]
-        self.assertEqual(item['content_version'], version)
+        versions = [
+            'latest',
+            '8549-7323',
+        ]
+
+        for version in versions:
+            resp = await self.api.release_notes(type='content',
+                                                version=version)
+            self.assertEqual(resp.status, 200)
+            x = await resp.json()
+            self.assertEqual(x['message'], 'Successful')
+            self.assertTrue(x['success'])
+            self.assertEqual(x['count'], 1)
+            self.assertEqual(x['count'], len(x['data']))
+            item = x['data'][0]
+            if version != 'latest':
+                self.assertEqual(item['content_version'], version)
--- tests/test_noaio_release_notes.py
+++ tests/test_noaio_release_notes.py
@@ -5,14 +5,20 @@ from . import mixin
 
 class ThreatVaultApiTest(mixin.Mixin, unittest.TestCase):
     def test_01(self):
-        version = '8549-7323'
-        resp = self.api.release_notes(type='content',
-                                      version=version)
-        self.assertEqual(resp.status_code, 200)
-        x = resp.json()
-        self.assertEqual(x['message'], 'Successful')
-        self.assertTrue(x['success'])
-        self.assertEqual(x['count'], 1)
-        self.assertEqual(x['count'], len(x['data']))
-        item = x['data'][0]
-        self.assertEqual(item['content_version'], version)
+        versions = [
+            'latest',
+            '8549-7323',
+        ]
+
+        for version in versions:
+            resp = self.api.release_notes(type='content',
+                                          version=version)
+            self.assertEqual(resp.status_code, 200)
+            x = resp.json()
+            self.assertEqual(x['message'], 'Successful')
+            self.assertTrue(x['success'])
+            self.assertEqual(x['count'], 1)
+            self.assertEqual(x['count'], len(x['data']))
+            item = x['data'][0]
+            if version != 'latest':
+                self.assertEqual(item['content_version'], version)
